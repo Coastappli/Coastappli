a65d3e7f31c01b1b93bab878dcfd4c41645f15ff
Nico-late <nicolasscholten@live.fr>
2019-12-17 23:14:49 +0100

    Résolution problème qualité photo

--- app/src/main/java/osirisc/coastappli/MethodMainActivity.java
+++ app/src/main/java/osirisc/coastappli/MethodMainActivity.java
@@ -4,7 +4,10 @@ package osirisc.coastappli;
 import android.content.Intent;
 import android.graphics.Bitmap;
 
+import android.graphics.BitmapFactory;
+import android.net.Uri;
 import android.os.Bundle;
+import android.os.Environment;
 import android.provider.MediaStore;
 import android.view.View;
 import android.widget.Button;
@@ -13,11 +16,14 @@ import android.widget.LinearLayout;
 import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.content.FileProvider;
 import androidx.viewpager.widget.ViewPager;
 
 import com.google.android.material.tabs.TabLayout;
 
 import java.io.ByteArrayOutputStream;
+import java.io.File;
+import java.io.IOException;
 import java.text.SimpleDateFormat;
 import java.util.Date;
 import java.util.Locale;
@@ -36,7 +42,7 @@ public class MethodMainActivity extends AppCompatActivity {
     private String coastType;
     private String INEC;
     private int erosionDistanceMesureBool;
-    private Bitmap imageBitmap;
+    private Bitmap bitmap;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -60,26 +66,77 @@ public class MethodMainActivity extends AppCompatActivity {
         }
     }
 
-    static final int REQUEST_IMAGE_CAPTURE = 1;
+    String currentPhotoPath;
+
+    private File createImageFile() throws IOException {
+        // Create an image file name
+        String timeStamp = new SimpleDateFormat("yyyyMMdd_HHmmss").format(new Date());
+        String imageFileName = "JPEG_" + timeStamp + "_";
+        File storageDir = getExternalFilesDir(Environment.DIRECTORY_PICTURES);
+        File image = File.createTempFile(
+                imageFileName,  /* prefix */
+                ".jpg",         /* suffix */
+                storageDir      /* directory */
+        );
+
+        // Save a file: path for use with ACTION_VIEW intents
+        currentPhotoPath = image.getAbsolutePath();
+        return image;
+    }
+
+    static final int REQUEST_TAKE_PHOTO = 1;
 
     public void dispatchTakePictureIntent(View view) {
         Intent takePictureIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+        // Ensure that there's a camera activity to handle the intent
         if (takePictureIntent.resolveActivity(getPackageManager()) != null) {
-            startActivityForResult(takePictureIntent, REQUEST_IMAGE_CAPTURE);
+            // Create the File where the photo should go
+            File photoFile = null;
+            try {
+                photoFile = createImageFile();
+            } catch (IOException ex) {
+                // Error occurred while creating the File
+            }
+            // Continue only if the File was successfully created
+            if (photoFile != null) {
+                Uri photoURI = FileProvider.getUriForFile(this,
+                        "osirisc.coastappli.fileprovider",
+                        photoFile);
+                takePictureIntent.putExtra(MediaStore.EXTRA_OUTPUT, photoURI);
+                startActivityForResult(takePictureIntent, REQUEST_TAKE_PHOTO);
+            }
+        }
+    }
+
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        if (requestCode == REQUEST_TAKE_PHOTO && resultCode == RESULT_OK) {
+
             Button buttonValidate = findViewById(R.id.buttonValidate);
             LinearLayout noteLayout = findViewById(R.id.NoteLayout);
             noteLayout.setVisibility(VISIBLE);
             buttonValidate.setVisibility(VISIBLE);
-        }
-    }
 
-    @Override
-    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
-        if (requestCode == REQUEST_IMAGE_CAPTURE && resultCode == RESULT_OK) {
-            Bundle extras = data.getExtras();
-            imageBitmap = (Bitmap) extras.get("data");
             ImageView imageViewPhoto = findViewById(R.id.imageViewPhoto);
-            imageViewPhoto.setImageBitmap(imageBitmap);
+            int targetW = imageViewPhoto.getWidth();
+            int targetH = imageViewPhoto.getHeight();
+
+            // Get the dimensions of the bitmap
+            BitmapFactory.Options bmOptions = new BitmapFactory.Options();
+            bmOptions.inJustDecodeBounds = true;
+
+            int photoW = bmOptions.outWidth;
+            int photoH = bmOptions.outHeight;
+
+            // Determine how much to scale down the image
+            int scaleFactor = Math.min(photoW/targetW, photoH/targetH);
+
+            // Decode the image file into a Bitmap sized to fill the View
+            bmOptions.inJustDecodeBounds = false;
+            bmOptions.inSampleSize = scaleFactor;
+            bmOptions.inPurgeable = true;
+
+            bitmap = BitmapFactory.decodeFile(currentPhotoPath, bmOptions);
+            imageViewPhoto.setImageBitmap(bitmap);
             imageViewPhoto.setVisibility(VISIBLE);
         }
     }
@@ -93,11 +150,12 @@ public class MethodMainActivity extends AppCompatActivity {
         TextView noteText = findViewById(R.id.editTextNote);
         mesure.setNotes((noteText.getText()).toString());
         mesure.setUser(getResources().getString(R.string.notImplemented));
+        ByteArrayOutputStream baos = new ByteArrayOutputStream();
 
-        ByteArrayOutputStream stream = new ByteArrayOutputStream();
-        imageBitmap.compress(Bitmap.CompressFormat.PNG, 100, stream);
-        byte[] byteArray = stream.toByteArray();
-        mesure.setPhoto(byteArray);
+        //Photo is compressed to 60% in order to be a smaller byte[]: it doesn't work if it's too big
+        bitmap.compress(Bitmap.CompressFormat.JPEG, 60 , baos);
+        byte[] b = baos.toByteArray();
+        mesure.setPhoto(b);
         DatabaseAssistant databaseAssistant = new DatabaseAssistant(this);
         databaseAssistant.addMesureErosionDistance(mesure);
         MethodMainActivity.this.finish();
@@ -111,6 +169,7 @@ public class MethodMainActivity extends AppCompatActivity {
         myPlaceIntent.putExtra("erosionDistanceMesureBool", erosionDistanceMesureBool);
         PlaceMainActivity.getInstance().finish();
         MethodMainActivity.this.startActivity(myPlaceIntent);
+        new File(currentPhotoPath).delete();
     }
 
     public Double getMarkerLatitude() {
@@ -122,6 +181,6 @@ public class MethodMainActivity extends AppCompatActivity {
     }
 
     public Bitmap getImageBitmap() {
-        return imageBitmap;
+        return bitmap;
     }
 }
